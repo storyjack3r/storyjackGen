acaf48caf1673e6a49c8c044151be89d151cf5e3
David Jackson <davidjackson@MacBook-Pro.local>
2017-03-11 22:41:35 +0000

    Multiple fields and sentence maker
    User can now fill multiple fields and either use each field separately
    or use a button to create a composition.

--- rand-gen.js
+++ rand-gen.js
@@ -1,11 +1,11 @@
-	console.log('boo hoo!');
+	console.log('Javascript rockets are functioning');
 	var storyLog ="";
 	var lastChoice=-1;
+	var lastChoice = [ -1, -1, -1, -1, -1, -1 ];
 
-	document.addEventListener('DOMContentLoaded', function () {
-
-	document.getElementById("noun-submit").addEventListener("click", function () {
-		var allText = document.getElementById("oldText").value;
+	
+function randomiser(listNumber){
+		var allText = document.getElementsByClassName("oldText")[listNumber].value;
 		
 		allText = allText.trim();
 		
@@ -23,21 +23,46 @@
 		var numOfChoices = (choiceArray.length);
 
 	    var x=Math.floor(Math.random()*numOfChoices);
-		console.log('last choice = ' + lastChoice);
+		console.log('last choice = ' + lastChoice[listNumber]);
 	    console.log('x = ' + x);
 	    //trying to make less repetition in choice
-	    while (x==lastChoice){
+	    	if (numOfChoices!=1){
+	    		// console.log('Theres more than one choice')
+	    while (x==lastChoice[listNumber]){
+	    
 	    x=Math.floor(Math.random()*numOfChoices);	
+
 	    }
-	    lastChoice=x;
+	    }
+	    lastChoice[listNumber]=x;
 	    storyLog = storyLog + " " + choiceArray[x];
 		document.getElementById("newText").value = storyLog;
-	});
 	
-	document.getElementById("tf-reset").addEventListener("click", function () {
+}
+
+function resetter(){
 		document.getElementById("oldText").value = "";
 		document.getElementById("newText").value = "";
 		document.getElementById("oldText").focus();
-	});
 	
-});
+}
+function storyResetter(){
+		storyLog= "";
+		document.getElementById("newText").value = "";
+	
+}
+
+
+function sentenceBuilder(){
+	console.log('sentence Builder is running');
+	var thisSentence = randomiser(0) + " " + randomiser(1) + " "+ randomiser(2);
+	// console.log(thisSentence);
+	// var editedStoryLog = document.getElementById("newText").value;
+	// console.log(editedStoryLog);
+	// storyLog = editedStoryLog + ".. " + thisSentence;
+	// document.getElementById("newText").value = storyLog;
+
+}
+
+
+
